d5cd89c8761ddbaa45623efea6a6ab6fc2282ca1
hyunchul30 <hyunchul30@yahoo.com>
2019-09-05 11:58:21 -0700

    learn

---
+++ index.js
@@ -0,0 +1,8 @@
+const add = require('./add');
+
+console.log(add(2,5));
+
+module.exports=function add(one, two) {
+return one + two;
+
+}
---
+++ src/triangle.js
@@ -0,0 +1,39 @@
+function Triangle(side1, side2, side3) {
+  this.side = side1;
+  this.side = side2;
+  this.side = side3;
+}
+
+Triangle.prototype.checkType = function() {
+};
+
+Triangle.prototype.checkType = function() {
+
+};
+
+class Triangle {
+  constructor(side1, side2, side3) {
+    this.side1 = side1;
+    this.side2 = side2;
+    this.side3 = side3;
+  }
+
+  checkType() {
+    //Function body goes here.
+  }
+};
+
+class Shape {
+
+}
+
+class Triangle extends Shape {
+
+}
+
+class Triangle {
+
+  checkType() {
+    let variableScopedToMethod = 0;
+  }
+};
